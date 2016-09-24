5c8c1b39b02d2904c4b4ac7a555069e638bff0c8
Amarishwer1 <S528121@mail.nwmissouri.edu>
2016-09-23 23:27:40 -0500

    Add files via upload

---
+++ A02Edam/main.js
@@ -0,0 +1,24 @@
+function calculateBmi() {
+var weight = document.bmiForm.weight.value
+var height = document.bmiForm.height.value
+if(weight > 0 && height > 0){	
+var finalBmi = weight/(height/100*height/100)
+document.bmiForm.bmi.value = finalBmi
+if(finalBmi < 18.5){
+document.bmiForm.meaning.value = "That you are too thin."
+document.getElementById('x').innerHTML= '<img src="vtimxrajzbuard4hsj78.jpg" />'; 
+}
+if(finalBmi > 18.5 && finalBmi < 25){
+document.bmiForm.meaning.value = "That you are healthy."
+}
+if(finalBmi > 25){
+document.bmiForm.meaning.value = "That you have overweight."
+}
+}
+else{
+alert("Please enter valid numbers")
+}
+}
+function changingImg(){
+    document.getElementById("y").src="./uw.png";
+}
